29fed421995f42beb5fa5455d315f581c29db0a2
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-02-04 06:44:12 -0300

    Trabalhando nas melhorais

--- 02 - React-para-iniciantes/04-webpack-e-babel/app/src/index.js
+++ 02 - React-para-iniciantes/04-webpack-e-babel/app/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import ReactDOM from "react-dom";
+import React from "react";
+
+function App() {
+  return React.createElement(
+    "a",
+    { href: "https://www.origamid.com" },
+    "Origamid"
+  );
+}
+
+ReactDOM.render(App(), document.getElementById("root"));
