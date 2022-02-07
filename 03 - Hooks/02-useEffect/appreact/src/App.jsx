d3d3a0513a9a4bda6d59b455c99de10ed089be74
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-02-07 14:47:51 -0300

    Praticando com o exercício de useEffect

--- 03 - Hooks/02-useEffect/appreact/src/App.jsx
+++ 03 - Hooks/02-useEffect/appreact/src/App.jsx
@@ -1,39 +1,14 @@
 import React from "react";
-import Produto from "./components/Produto";
-
 // [ ] Quando o usuário clicar em um dos botões, faça um fetch do Produto
 // [ ] https://ranekapi.dev/json/api/produto
 // [ ] Mostre o nome e preço na tela (separe essa informação em um componente produto)
-// [ ] Defina o produto clicado como uma preferência do usuário no localStore
+// [x] Defina o produto clicado como uma preferência do usuário no localStore
 // [ ] Quando o usuário entrar no site, se exister um produto no localStore
 
 const App = () => {
-  const [produto, setProduto] = React.useState(null);
-
-  // Existe produto no localStore
-  React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem("produto");
-    if (produtoLocal !== null) setProduto(produtoLocal);
-  }, []);
-
-  // Adicionar produto no localStore
-  React.useEffect(() => {
-    if (produto !== null) window.localStorage.setItem("produto", produto);
-  }, [produto]);
-
-  // Setar produto clicado pelo o usuário
-  function handleClick({ target }) {
-    setProduto(target.innerText);
-  }
   return (
     <div>
-      <h1>Preferência: {produto} </h1>
-      <button onClick={handleClick} style={{ marginRight: "1rem" }}>
-        notebook
-      </button>
-      <button onClick={handleClick}>smartphone</button>
-
-      <Produto produto={produto} />
+      <h1>Preparando</h1>
     </div>
   );
 };
--- 03 - Hooks/02-useEffect/appreact/src/components/Produto.jsx
+++ 03 - Hooks/02-useEffect/appreact/src/components/Produto.jsx
@@ -1,22 +1,9 @@
 import React from "react";
 
-const Produto = ({ produto }) => {
-  const [dados, setDados] = React.useState(null);
-
-  React.useEffect(() => {
-    if (produto !== null)
-      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
-        .then((response) => response.json())
-        .then((json) => setDados(json));
-  }, [produto]);
-
-  if (dados === null) return null;
-  return (
-    <div>
-      <h1>{dados.nome}</h1>
-      <p>R$ {dados.preco}</p>
-    </div>
-  );
+const Produto = () => {
+  <div>
+    <h1>Produto</h1>
+  </div>;
 };
 
 export default Produto;
