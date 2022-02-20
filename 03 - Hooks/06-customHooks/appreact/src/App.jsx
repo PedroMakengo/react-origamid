dcdc6fbaed4825ebf476f2e907a580803ff41433
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-02-20 06:20:48 -0300

    Trabalhando com custom hooks

--- 03 - Hooks/06-customHooks/appreact/src/App.jsx
+++ 03 - Hooks/06-customHooks/appreact/src/App.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { GlobalStorage } from "./context/GlobalContext";
-import Produto from "./components/Produto";
-import Button from "./components/Button";
+import useFetch from "./useFetch";
+import useLocalStorage from "./useLocalStorage";
 
 const App = () => {
-  return (
-    <GlobalStorage>
-      <Produto />
-      <Button />
-    </GlobalStorage>
-  );
+  const [produto, setProduto] = useLocalStorage("produto", "");
+  const { request, data, loading } = useFetch();
+
+  React.useEffect(() => {
+    request(`https://ranekapi.origamid.dev/json/api/produto`);
+  }, []);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
+  if (loading) return <div>Carregando...</div>;
+  if (data)
+    return (
+      <div>
+        <p>Produto preferido: {produto} </p>
+        <button onClick={handleClick}>notebook</button>
+        <button onClick={handleClick}>smartphone</button>
+
+        {data.map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  else return null;
 };
 export default App;
---
+++ 03 - Hooks/06-customHooks/appreact/src/useFetch.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  async function request(url, options) {
+    try {
+      setLoading(true);
+      const response = await fetch(url, options);
+      const json = await response.json();
+      setData(json);
+    } catch (erro) {
+      setError(erro);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { data, error, loading, request };
+};
+
+export default useFetch;
