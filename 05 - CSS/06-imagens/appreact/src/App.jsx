f4d135344c7d9a25045ff490464b0f9293ec5e00
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-02-27 20:11:57 -0300

    Terminando a parte prática do module de css

--- 05 - CSS/06-imagens/appreact/src/App.jsx
+++ 05 - CSS/06-imagens/appreact/src/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import Slide from "./components/Slide";
+import "./App.css";
+
+import foto from "./img/foto.jpg";
+import DogSvg from "./DogSvg";
 
 const App = () => {
-  const slides = [
-    { id: "slide1", text: "Slide 1" },
-    { id: "slide2", text: "Slide 2" },
-    { id: "slide3", text: "Slide 3" },
-  ];
+  const [olho, setOlho] = React.useState(0);
+
+  function handleClick() {
+    for (let i = 0; i < 6; i++) {
+      setTimeout(() => {
+        setOlho(i);
+      }, 100 * i);
+    }
+  }
   return (
     <div>
-      <Slide slides={slides} />
+      <p className="fundo" onClick={handleClick}></p>
+      <DogSvg color="#84e" olho={olho} />
+      <img src={foto} alt="Cachorro" />
     </div>
   );
 };
