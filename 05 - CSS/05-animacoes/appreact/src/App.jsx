8ba9e0eddad0a19dd106405e9369aac2e9cba67a
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-02-27 19:51:49 -0300

    Terminando a parte de css do curso

---
+++ 05 - CSS/05-animacoes/appreact/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Slide from "./components/Slide";
+
+const App = () => {
+  const slides = [
+    { id: "slide1", text: "Slide 1" },
+    { id: "slide2", text: "Slide 2" },
+    { id: "slide3", text: "Slide 3" },
+  ];
+  return (
+    <div>
+      <Slide slides={slides} />
+    </div>
+  );
+};
+export default App;
