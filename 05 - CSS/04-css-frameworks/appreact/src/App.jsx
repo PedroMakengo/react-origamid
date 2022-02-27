53406b3f88c5e136cc6431a249394e20f30e2bbe
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-02-27 19:13:50 -0300

    Terminando a aula sobre o uso de framework bootstrap

--- 05 - CSS/04-css-frameworks/appreact/src/App.jsx
+++ 05 - CSS/04-css-frameworks/appreact/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+// import Card from "react-bootstrap/Card";
+
 const App = () => {
   return (
-    <div>
-      <h1>Olá Mundo</h1>
-    </div>
+    <Card bg="dark" text="white" className="m-5" style={{ maxWidth: "18rem" }}>
+      <Card.Header>Notebook</Card.Header>
+      <Card.Body>
+        <Card.Title>R$ 2500</Card.Title>
+        <Card.Text>
+          Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16 gb.
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 export default App;
--- 05 - CSS/04-css-frameworks/appreact/src/index.jsx
+++ 05 - CSS/04-css-frameworks/appreact/src/index.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Instalando o bootstrap
+// npm i bootstrap
+// import "bootstrap/dist/js/bootstrap.bundle.min.js"
+// import "bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
