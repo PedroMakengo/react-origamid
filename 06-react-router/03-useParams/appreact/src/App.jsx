99a906155632580c50228fe7e31e2795322406a3
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-04 18:11:46 -0300

    Trabalhando com o useParams

---
+++ 06-react-router/03-useParams/appreact/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NaoEncontrada from "./pages/NaoEncontrada";
+import Sobre from "./pages/Sobre";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NaoEncontrada />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
