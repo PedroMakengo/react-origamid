5e2ce1ec67853452aded20f57c5643e4d0ba5bbe
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-08 01:40:05 -0300

    Trabalhando com o prototypes do react

--- 07-mais-react/01-prototypes/appreact/src/App.jsx
+++ 07-mais-react/01-prototypes/appreact/src/App.jsx
@@ -1,29 +1,10 @@
 import React from "react";
-import "./App.css";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Produtos from "./components/Produtos";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Contatos from "./components/Contatos";
-import Produto from "./components/Produto";
+import Button from "./components/Button";
 
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Produtos />} />
-            <Route path="produto/:id" element={<Produto />} />
-            <Route path="contatos" element={<Contatos />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+    <div>
+      <Button width={300}>Clique aqui</Button>
     </div>
   );
 };
