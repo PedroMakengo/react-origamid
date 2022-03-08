9ffe58079351a2b9fe44ecfe2f21c0f6b5c5390a
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-08 15:32:32 -0300

    Trabalhando com o memo

---
+++ 07-mais-react/03-memo/appreact/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Contato from "./components/Contato";
+
+const App = () => {
+  return (
+    <div>
+      <Contato />
+      {/* Pesquisar sobre o react lazy e suspense */}
+    </div>
+  );
+};
+export default App;
