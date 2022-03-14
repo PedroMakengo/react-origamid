b6d011eaab8b65ec4fdb6a5f4c1b66643835063e
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-14 19:32:17 -0300

    Trabalhando projeto-final

--- 08-projeto-final/03-login-routes/src/Components/Forms/Input/index.jsx
+++ 08-projeto-final/03-login-routes/src/Components/Forms/Input/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./index.module.css";
 
-const Input = () => {
-  return <Input className={styles.input} />;
+const Input = ({ label, type, name }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input id={name} name={name} className={styles.input} type={type} />
+    </div>
+  );
 };
 
 export default Input;
