4181d36b9807a25aa15c02068791a64ce14dca67
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-14 18:38:55 -0300

    Trabalhando na parte de consumo da api

--- 08-projeto-final/03-login-routes/src/Components/Login/LoginForm/index.jsx
+++ 08-projeto-final/03-login-routes/src/Components/Login/LoginForm/index.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const LoginForm = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch(`https://dogsapi.origamid.dev/json/jwt-auth/v1/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => console.log(json));
+  }
+
   return (
-    <div>
-      <h1>LoginCreate</h1>
-    </div>
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          valeu={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
   );
 };
 
