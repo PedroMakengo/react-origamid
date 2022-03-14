40bab7460e613365e21a78aee721495700bacc10
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-14 15:52:59 -0300

    Criado inicialmente o meu header

--- 08-projeto-final/02-header/src/Components/Header/index.jsx
+++ 08-projeto-final/02-header/src/Components/Header/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./index.module.css";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Dogs } from "../../Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login"> Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Home">
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
