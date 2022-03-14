04a0decf9e853f76e1a7cbdaa59754d7c9ce1454
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-14 19:23:14 -0300

    Trabalhando com FormsComponent

---
+++ 08-projeto-final/03-login-routes/src/Components/Forms/Button/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Button = () => {
+  return <button className={styles.button}></button>;
+};
+
+export default Button;
---
+++ 08-projeto-final/03-login-routes/src/Components/Forms/Input/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Input = () => {
+  return <Input className={styles.input} />;
+};
+
+export default Input;
