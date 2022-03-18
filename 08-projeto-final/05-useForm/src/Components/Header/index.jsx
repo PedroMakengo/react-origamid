08eddb3086f05ef0ede8b430acf4208c8559c94f
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-18 17:15:24 -0300

    aprendendo useForms

---
+++ 08-projeto-final/05-useForm/src/Components/Header/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./index.module.css";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Dogs } from "../../Assets/dogs.svg";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Home">
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ 08-projeto-final/05-useForm/src/Components/Login/LoginPasswordLost/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPasswordLost = () => {
+  return (
+    <div>
+      <h1>LoginForm</h1>
+    </div>
+  );
+};
+
+export default LoginPasswordLost;
