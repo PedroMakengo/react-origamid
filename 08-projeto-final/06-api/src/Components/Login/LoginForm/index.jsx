4b507764f1c98df51036299d4a3750f60f7a10e4
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-19 14:05:32 -0300

    Trabalhando com api

--- 08-projeto-final/06-api/src/Components/Login/LoginForm/index.jsx
+++ 08-projeto-final/06-api/src/Components/Login/LoginForm/index.jsx
@@ -4,27 +4,42 @@ import { Link } from "react-router-dom";
 import Input from "../../Forms/Input";
 import Button from "../../Forms/Button";
 import useForm from "../../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../../api";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(e) {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+
+    console.log(json);
+  }
+
+  async function handleSubmit(e) {
     e.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch(`https://dogsapi.origamid.dev/json/jwt-auth/v1/token`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => console.log(json));
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
     }
   }
 
@@ -32,9 +47,9 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="email" name="username" {...username} />
+        <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button disabled>Entrar</Button>
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
